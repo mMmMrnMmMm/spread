cd01c0f2214b299c49cbaed6f5202575632333ae
mMmMrnMmMm <32131611+mMmMrnMmMm@users.noreply.github.com>
2017-11-13 14:55:47 -0500

    Added contents, changed how next map is generated
    note: add classes for cells, maybe items.

--- test.py
+++ test.py
@@ -6,7 +6,9 @@ def fillcell(): #needs work
     return {"name":"empty", #cell name
             "display":"M", #what character it is displayed as
             "state":0,  #what the cell is currently displaying (0= self, 1 = contents, 2 = direction)
-            "direction":0} #what direction it is pointing in
+            "direction":0, #what direction it is pointing in
+            "contents":[] #what items are on the cell
+            } 
 
 def createmap(x,y):
     Map = []
@@ -30,7 +32,12 @@ def prettyprint():
 
 def tick():
     nextmap =  createmap(len(wmap),len(wmap[0]))
-        
+    for y in len(wmap)-1:
+        for x in len(wmap[0])-1:
+            cell = wmap[y][x]
+            if cell["name"] == "mine":
+                
+                
 
 wmap = createmap(6,7)
 prettyprint()
