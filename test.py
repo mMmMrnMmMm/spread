e8be3d600a0544d17e897207015da6d8d77353d6
mMmMrnMmMm <32131611+mMmMrnMmMm@users.noreply.github.com>
2017-11-13 14:12:43 -0500

    fixed x&y

--- test.py
+++ test.py
@@ -4,16 +4,16 @@ wmap = []
 # 3 4 5
 def fillcell(): #needs work
     return {"name":"empty", #cell name
-            "display":" ", #what character it is displayed as
+            "display":"M", #what character it is displayed as
             "state":0,  #what the cell is currently displaying (0= self, 1 = contents, 2 = direction)
             "direction":0} #what direction it is pointing in
 
 def createmap(x,y):
     Map = []
     width = []
-    for i in range(y):
-        width.append(fillcell())
     for i in range(x):
+        width.append(fillcell())
+    for i in range(y):
         Map.append(width)
     return Map
 
@@ -22,7 +22,7 @@ def prettyprint():
     while len(printhelp) < len(wmap[0]):
         printhelp.append("\n")
     printstr = ""
-    for i in range(len(wmap)):
+    for i in range(len(wmap)-1):
         for j in wmap[i]:
             printstr = printstr+j["display"]
         printstr = printstr+ "    "+ printhelp[i]
