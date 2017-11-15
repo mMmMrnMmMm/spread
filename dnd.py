f75c65ad77754ff5596b5a4fc00d4271b4bccb72
mMmMrnMmMm <32131611+mMmMrnMmMm@users.noreply.github.com>
2017-11-15 07:56:59 -0500

    objectified the map, somewhat

--- dnd.py
+++ dnd.py
@@ -1,1 +1,12 @@
-import test
+import test as mu
+
+cells = [["empty", " "]
+         ["mountain", "Λ"]
+         ["water","~"]
+         []
+         []
+         []]
+
+Map = mu.createmap(20, 20)
+mu.phelp = []
+mu.prettyprint(Map)
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-wmap = []
+phelp = ["h to hide help \n", "i j k l for movement \n"]
 # 1 8 7
 # 2 0 6
 # 3 4 5
@@ -8,7 +8,14 @@ def fillcell(): #needs work
             "state":0,  #what the cell is currently displaying (0= self, 1 = contents, 2 = direction)
             "direction":0, #what direction it is pointing in
             "contents":[] #what items are on the cell
-            } 
+            }
+
+class cell():
+    def __init__(self, name, display):
+        self.name
+        self.display
+        self.contents = []
+        self.submap = []
 
 def createmap(x,y):
     Map = []
@@ -19,27 +26,17 @@ def createmap(x,y):
         Map.append(width)
     return Map
 
-def prettyprint():
-    printhelp = ["h to hide help \n", "i j k l for movement \n"]
-    while len(printhelp) < len(wmap[0]):
+def prettyprint(Map):
+    printhelp = phelp
+    while len(printhelp) < len(Map[0]):
         printhelp.append("\n")
     printstr = ""
-    for i in range(len(wmap)-1):
-        for j in wmap[i]:
+    for i in range(len(Map)-1):
+        for j in Map[i]:
             printstr = printstr+j["display"]
         printstr = printstr+ "    "+ printhelp[i]
     print(printstr)
 
-def tick():
-    nextmap =  createmap(len(wmap),len(wmap[0]))
-    for y in len(wmap)-1:
-        for x in len(wmap[0])-1:
-            cell = wmap[y][x]
-            if cell["name"] == "mine":
-                pass
                 
-                
-
-wmap = createmap(6,7)
-prettyprint()
+            
 
