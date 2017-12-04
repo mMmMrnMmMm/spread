2049df7977ffff4f5b0e3e2fff03527a9332cdaa
mMmMrnMmMm <32131611+mMmMrnMmMm@users.noreply.github.com>
2017-12-04 13:48:08 -0500

    split up some functions
    modfied createmap, fillcell is now a default, and not the only option for cell generation.
    cells know their position.
    a start to the definition of neighborhoods and map generation.

--- dnd.py
+++ dnd.py
@@ -1,4 +1,6 @@
+import random
 import test as mu
+
 
 mu.cells = [["empty", " "],
          ["mountain", "Λ"],
@@ -6,6 +8,18 @@ mu.cells = [["empty", " "],
          [],
          []]
 
-Map = mu.createmap(4, 3)
+mapheight = 8
+mapwidth = 4
+def wgen(m):
+    #elevation
+    #0000+00000+  - 11*5 = 55/10 = 5 = 5
+    #0+000000000
+    #0000000+000
+    #00+00000000
+    #00000000000
+    for i in range(random.randint(1,mapheight*mapwidth/10)):
+        pass
+    
+Map = mu.createmap(mapwidth, mapheight)
 mu.phelp = []
 mu.prettyprint(Map)
--- test.py
+++ test.py
@@ -4,25 +4,35 @@ cells = [["empty", " "]
 # 1 8 7
 # 2 0 6
 # 3 4 5
-
 class cell():
-    def __init__(self, cel_id):
+    def celcheck(self,cel_id):
         if type(cel_id) == str:
             cel = cells.index(cel_id)
         else:
             cel = cel_id
+        return cel
+    def __init__(self, cel_id, x, y):
+        cel = self.celcheck(cel_id)
         self.name = cells[cel][0]
         self.display = cells[cel][1]
         self.contents = []
         self.submap = []
         self.state = 0
+        self.x = x
+        self.y = y
+    def changetype(self, typ):
+        self.name = cells[typ][0]
+        self.display = cells[typ][1]
+    def setneighborhood(self, typ, size, context):
+        if typ == 0 or typ == "square":
+            self.neighborhood = [[] for i in range(size*2+1)]
+            
 
+def fillcell(x, y): #needs work
+    return cell(0, x, y)
 
-def fillcell(): #needs work
-    return cell(0)
-
-def createmap(x,y, cellgen = fillcell()):
-    return [[cellgen for i in range(x)] for j in range(y)]
+def createmap(x,y, cellgen = fillcell):
+    return [[cellgen(x = i, y = j) for i in range(x)] for j in range(y)]
 
 def prettyprint(Map):
     printhelp = phelp
@@ -34,7 +44,3 @@ def prettyprint(Map):
             printstr = printstr+j.display
         printstr = printstr+ "    "+ printhelp[i]
     print(printstr)
-
-                
-            
-
